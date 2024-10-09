30bff0e0ec273ebbfbcab76fe932435cf1d777f1
dreathwings <warren.privat0@gmail.com>
2024-10-09 15:32:23 +0200

    BBBB

--- serveur.py
+++ serveur.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
-app = Flask('mission',static_folder='/mission/static')
+#app = Flask('mission')
+app = Flask('mission',static_url_path='/mission/static/')
 app.config.update(
                   TEMPLATES_AUTO_RELOAD=True)
 
 
-@app.route("/")
+@app.route("/mission")
 def ordre():
     return render_template('new_order.html')
 
 # Running the API
 if __name__ == "__main__":
-    app.run(port="6969",debug=True)
+	with app.app_context():
+        	for rule in app.url_map.iter_rules():
+            		print(f"{rule.endpoint}: {rule.methods} - {rule}")
+	app.run(port="6969",debug=True)
