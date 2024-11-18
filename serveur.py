4fd417237088973e210ff3fff18cb250b35c0fec
Dreathwings <warren.privat0@gmail.com>
2024-11-18 13:41:51 +0100

    sasaas

--- serveur.py
+++ serveur.py
@@ -21,7 +21,7 @@ def oauth():
                   'service':"http://geii.iut.u-bordeaux.fr/mission/oauth"}
         print(f"Ticket :{request.values['ticket']}")
         RESP = REQ.get(url = "https://cas.u-bordeaux.fr/cas/serviceValidate",params=PARAMS)
-        id = str(RESP.content).split('cas:user')
+        id = str(RESP.content).split('cas:user')[1].removeprefix('>').removesuffix("</")
         return id
     else:
         return redirect("https://cas.u-bordeaux.fr/cas/login?service=http://geii.iut.u-bordeaux.fr/mission/oauth")
