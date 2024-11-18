ea320ea34eade14559d4732532dc8b3ae4552439
Dreathwings <warren.privat0@gmail.com>
2024-11-18 13:11:39 +0100

    asasa

--- serveur.py
+++ serveur.py
@@ -20,7 +20,7 @@ def oauth():
         PARAMS = {"ticket":request.values['ticket']}
         print(f"Ticket :{request.values['ticket']}")
         ID = REQ.get(url = "https://cas.u-bordeaux.fr/cas/validate",params=PARAMS)
-        print(ID)
+        print(ID.raw)
         return str(ID._content)
     else:
         return redirect("https://cas.u-bordeaux.fr/cas/login?service=http://geii.iut.u-bordeaux.fr/mission/oauth")
