e69231e6a926931a456b95c4d0db96109f37633f
Dreathwings <warren.privat0@gmail.com>
2024-11-19 15:57:08 +0100

    azdazdza

--- serveur.py
+++ serveur.py
@@ -126,7 +126,7 @@ def create_new_mission():
     try:
         cur.execute("INSERT INTO mission.ordre_mission(ID,NOM,PRENOM,DATE_AJD,NOM_MISSION,PAYS_MISSION,FRAIS,D_DEPART,D_RETOUR,TRANSPORT,LIEU,CODE_PTL,VILLE,HOTEL,PTDEJ,QUILL_URL) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",(ID.__repr__(),val["NOM"],val["PRENOM"],val["DATE_AJD"],val["NOM_MISSION"],PAYS,val["FRAIS"],val["D_DEPART"],val["D_RETOUR"],val["TRANSPORT"],val["LIEU"],val["CODE_PTL"],val["VILLE"],val["HOTEL"],val["PTDEJ"],'BOBO'))
         statu_crea = 0
-        cur.execute("INSERT INTO mission.suivi_mission(ID,ID_USER,NAME,DATE_CREA,STATUE) VALUES (?,?,?,?,?)",(ID.__str__(),user_id,nom,val["DATE_AJD"],statu_crea))
+        cur.execute("INSERT INTO mission.suivi_mission(ID,ID_USER,NAME,DATE_CREA,STATUE) VALUES (?,?,?,?,?)",(ID.__str__(),user_id,nom[0],val["DATE_AJD"],statu_crea))
         print(f"Ordre mission {ID} success")
     except mariadb.Error as e: 
         print(f"Error: {e}")
