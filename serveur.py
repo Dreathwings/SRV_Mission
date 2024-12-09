9135377cb0cd42cf1c0515a61e55aa5dbdb2c4b0
Dreathwings <warren.privat0@gmail.com>
2024-12-09 17:05:06 +0100

    azdazd

--- serveur.py
+++ serveur.py
@@ -133,7 +133,8 @@ def show_mission(id_mission):
     cur = DB.cursor()
     cur.execute(f"SELECT ID_USER , STATUE FROM suivi_mission WHERE ID ='{id_mission}'")
     data = oauth_user[request.cookies.get("SESSID")]
-    (user,BOB) = cur.fetchall()[0]
+    dimitri = cur.fetchall()[0]
+    (user,BOB) = dimitri
     if data[2] == "ADMIN" or data[2] == "GESTION" or data[1] == user:
         cur.execute(f"SELECT * FROM ordre_mission WHERE ID ='{id_mission}'")
         mission = list(item for item in cur.fetchall()[0])
