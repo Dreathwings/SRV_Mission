96c5f664d7500e624416ed83315f0d34732552a8
Dreathwings <warren.privat0@gmail.com>
2024-12-09 16:57:06 +0100

    zadzad

--- serveur.py
+++ serveur.py
@@ -133,12 +133,12 @@ def show_mission(id_mission):
     cur = DB.cursor()
     cur.execute(f"SELECT ID_USER , STATUE FROM suivi_mission WHERE ID ='{id_mission}'")
     data = oauth_user[request.cookies.get("SESSID")]
-    dimitri = list(item for item in cur.fetchall()[0])
+    dimitri = list(item for item in cur.fetchall())
     user = dimitri[0]
     BOB = dimitri[1]
     if data[2] == "ADMIN" or data[2] == "GESTION" or data[1] == user:
         cur.execute(f"SELECT * FROM ordre_mission WHERE ID ='{id_mission}'")
-        mission = list(item for item in cur.fetchall())
+        mission = list(item for item in cur.fetchall()[0])
         return render_template('order.html', Mission=mission, STAT=BOB)
         #return f"<html><body> <h1>  {id_mission} {mission}  </h1></body></html>"
     else:
